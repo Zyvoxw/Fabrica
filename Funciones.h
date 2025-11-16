c784a17c303817f63e3bc3ba77e01d0a2633eee0
Zyvoxw <zaskia.sunta@udla.edu.ec>
2025-11-15 22:55:36 -0500

    Update Funciones.h

--- Funciones.h
+++ Funciones.h
@@ -1,3 +1,6 @@
-void ingresarProductoRecursos(char productos[5][50], int cantidadesProductos[5], int *numProductos, char recursos[100][50], int disponibles[100], int *numRecursos);
+#pragma once
+
+void ingresarProductoRecursos(char productos[5][50], int *numProductos, char recursos[45][50], int disponibles[45], int *numRecursos);
 void registrarTiempoProcesos(char nombreProductos[][50],char nombreRecursos[][50],int *numProductos,int *numRecursos,int recursosNecesarios[][5], int tiempoProducto[]);
+
 
