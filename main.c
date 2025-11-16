c9ec06652e55582f2708617b9e6de416a7fea427
IvethCastro <iveth.castro@udla.edu.ec>
2025-11-15 19:10:22 -0500

    Refactor main function with menu options

--- main.c
+++ main.c
@@ -1,26 +1,55 @@
 #include <stdio.h>
+#include <string.h>
 #include "Funciones.h"
 
 int main()
 {
-    char productos[5][50];
-    char recursos[100][50];
-    int disponibles[100];
-
-    int numProductos;
+    char nombreProducto[5][50];
+    int cantR=0;
+    char nombreRecursos[10];
     int numRecursos;
+    
+    int opcionMenu;
+    do{
+        printf("------------MENU--------\n");
+        printf("1. Ingresar productos del catalogo e inventario\n");
+        printf("2. Editar Datos\n");
+        printf("3. Eliminar Datos\n");
+        printf("4. Solicitud del cliente\n");
+        printf("5. Salir\n");   
+        printf(">> \n");
+        printf("Ingrese la opcion que desea: \n");
+        scanf(" %d",&opcionMenu);
+        switch (opcionMenu)
+        {
+        case 1:
+            do{
+                printf("Ingrese el nombre del producto (1-5): ");
+                leercadena(nombreProducto,50);
+            }while ();
+        
+            printf("Ingrese el nombre del producto (1-5): ");
 
-    ingresarProductosYRecursos(productos, &numProductos, recursos, disponibles, &numRecursos);
-
-    printf("--- Productos ingresados ---\n");
-    for (int i = 0; i < numProductos; i++) {
-        printf("%d. %s\n", i + 1, productos[i]);
-    }
-
-    printf("--- Recursos ingresados ---\n");
-    for (int i = 0; i < numRecursos; i++) {
-        printf("%d. %s (Cantidad: %d)\n", i + 1, recursos[i], disponibles[i]);
-    }
+            
+            break;
+        case 2:
+            
+            break;
+        case 3:
+            
+            break;
+        case 4:
+            
+            break;
+        case 5:
+            printf("Saliendo...\n"); 
+            break;
+        
+        default:
+            break;
+        }
+    } while (opcionMenu!=6);
+    
 
     return 0;
 }
