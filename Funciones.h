b240ddd46094b38cb83408dc751168eb5b300669
IvethCastro <iveth.castro@udla.edu.ec>
2025-11-15 21:42:25 -0500

    Update function signatures in Funciones.h

--- Funciones.h
+++ Funciones.h
@@ -1,1 +1,2 @@
-void ingresarProductosYRecursos(char productos[5][50], int *numProductos, char recursos[100][50], int disponibles[100], int *numRecursos);
+void ingresarProductoRecursos(char productos[5][50], int cantidadesProductos[5], int *numProductos, char recursos[45][50], int disponibles[45], int *numRecursos);
+void registrarTiempoProcesos(char nombreProductos[][50],char nombreRecursos[][50],int *numProductos,int *numRecursos,int recursosNecesarios[][5], int tiempoProducto[]);
