2158c7fc56fa1438641de8f672bf3a4482b0c5e3
Zyvoxw <zaskia.sunta@udla.edu.ec>
2025-11-15 21:07:25 -0500

    Refactor product and resource input functions

--- Funciones.c
+++ Funciones.c
@@ -69,79 +69,8 @@ do {
         printf("%d - %s - cantidad disponible: %d\n", i + 1, recursos[i], disponibles[i]);
     }
 }
-#include <stdio.h>
-#include <string.h>
-#include "Funciones.h"
 
-int main() {
-    char productos[5][50];
-    int cantidadesProductos[5];      
-    char recursos[100][50];
-    int disponibles[100];             
-    int numProductos = 0, numRecursos = 0;
 
-    int opcionMenu;
-
-    do {
-        printf("\n------------ MENU --------\n");
-        printf("1. Ingresar productos del catalogo e inventario\n");
-        printf("2. Editar Datos\n");
-        printf("3. Eliminar Datos\n");
-        printf("4. Solicitud del cliente\n");
-        printf("5. Salir\n");   
-        printf("Ingrese la opcion que desea: ");
-        scanf(" %d", &opcionMenu);
-        while(getchar() != '\n'); // limpiar buffer
-
-        switch (opcionMenu) {
-            case 1:
-                
-                ingresarProductoRecursos(productos, cantidadesProductos, &numProductos, recursos, disponibles, &numRecursos);
-                break;
-            case 2:
-               
-                break;
-            case 3:
-               
-                break;
-            case 4:
-                
-                break;
-            case 5:
-                printf("Saliendo...\n"); 
-                break;
-            default:
-                printf("Opcion invalida. Intente de nuevo.\n");
-                break;
-        }
-    } while (opcionMenu != 5);
-
-    // Mostrar resumen final
-    printf("\n========= RESUMEN FINAL =========\n");
-
-    printf("------- PRODUCTOS INGRESADOS -------\n");
-    if (numProductos == 0) {
-        printf("No se ingresaron productos.\n");
-    } else {
-        for (int i = 0; i < numProductos; i++) {
-            printf("%d - %s - cantidad: %d\n", i + 1, productos[i], cantidadesProductos[i]);
-        }
-    }
-
-    printf("------- RECURSOS INGRESADOS -------\n");
-    if (numRecursos == 0) {
-        printf("No se ingresaron recursos.\n");
-    } else {
-        for (int i = 0; i < numRecursos; i++) {
-            printf("%d - %s - cantidad disponible: %d\n", i + 1, recursos[i], disponibles[i]);
-        }
-    }
-
-    printf("\nTotal de productos ingresados: %d\n", numProductos);
-    printf("Total de recursos ingresados: %d\n", numRecursos);
-
-    return 0;
-}
 
 
 
